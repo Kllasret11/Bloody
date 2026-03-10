b51d672d19fdcb4dbc055874e5118b48c237862f
Klllarett <yan.krivolapov20102003@gmail.com>
2026-03-10 21:23:29 +0500

    fix merge conflict

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import logging
-<<<<<<< HEAD
 from aiogram import executor
 
 import handlers
@@ -22,7 +21,7 @@ async def on_shutdown(dispatcher):
     await db.close()
 
     logging.info("Database closed")
-=======
+
 
 from aiogram import executor
 
@@ -39,18 +38,18 @@ async def on_startup(_: object) -> None:
 
 async def on_shutdown(_: object) -> None:
     await db.close()
->>>>>>> 89904677af75836394a197c014783c6ca9e14d81
+
     logging.info("Bot stopped")
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
+
     executor.start_polling(
         dp,
         skip_updates=True,
         on_startup=on_startup,
         on_shutdown=on_shutdown
     )
-=======
+
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
->>>>>>> 89904677af75836394a197c014783c6ca9e14d81
+
